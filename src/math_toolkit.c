dafc5496f02a18c119ee60e22682042182e5914d
XIANZU <0520hyx@gmail.com>
2025-08-20 02:23:23 +1000

    main file update

--- src/math_toolkit.c
+++ src/math_toolkit.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "module/GCD_calculator.h"
+#include "module/prime_factorization.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+    if (argc <= 1) {
+        fprintf(stderr, "usage: %s\n \t[-g] for GCD calculation\n \t[-f] for prime factorization\n", argv[0]);
         return 1;
     }
 
-    for (int i = 1; i < argc; i++) {
-        printf("Arg %d: %s\n", i, argv[i]);
+    if (strcmp(argv[1], "-g") == 0) {
+        GCD_calculator(argc - 2, &argv[2]);
+    } else if (strcmp(argv[1], "-f") == 0) {
+        prime_factorization(argc - 2, &argv[2]);
+    } else {
+        fprintf(stderr, "unkonwn option %s\nusage: %s\n \t[-g] for GCD calculation\n \t[-f] for prime factorization\n", argv[1], argv[0]);
     }
 
     return 0;
